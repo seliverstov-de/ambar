565cd174bfda4b626c66ca6504181f3868ac556a
Pascal Honegger <pascal.honegger@bluewin.ch>
2022-08-11 11:49:00 +0200

    Migrate to official mongo image

--- MongoDB/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "storage:" > /etc/mongod.conf
-echo "  wiredTiger:" >> /etc/mongod.conf
-echo "    engineConfig:" >> /etc/mongod.conf
-echo "      cacheSizeGB: ${cacheSizeGB}" >> /etc/mongod.conf
-
-exec mongod --config /etc/mongod.conf
